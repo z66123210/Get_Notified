a3014899aadd9d548ee4715f5d194ccacbcaf997
Raymond <z66123218@gmail.com>
2024-06-19 15:11:43 +1000

    front end in progress

---
+++ front_end/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LandingPage from './Pages/LandingPage/LandingPage';
+
+function App() {
+  return (
+    
+<LandingPage/> 
+  );
+}
+
+export default App;
---
+++ front_end/src/Routes/Routes.tsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+import ProtectedRoute from "./ProtectedRoute";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
+      {
+        path: "search",
+        element: (
+          <ProtectedRoute>
+            <SearchPage />
+          </ProtectedRoute>
+        ),
+      },
+      { path: "design-guide", element: <DesignGuide /> },
+      {
+        path: "company/:ticker",
+        element: (
+          <ProtectedRoute>
+            <CompanyPage />
+          </ProtectedRoute>
+        ),
+        children: [
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+          { path: "balance-sheet", element: <BalanceSheet /> },
+          { path: "cashflow-statement", element: <CashflowStatement /> },
+          { path: "historical-dividend", element: <HistoricalDividend /> },
+        ],
+      },
+    ],
+  },
+]);
